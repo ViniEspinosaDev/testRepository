a59c183ecab167e56ce14e606b444cbf8a1423b3
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2020-06-22 09:39:17 -0300

    Adicionando btn login

--- ProjetoTesteComandosGitHUb/Form1.Designer.cs
+++ ProjetoTesteComandosGitHUb/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProjetoTesteComandosGitHUb
 {
-    partial class Form1
+    partial class FrmMenu
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(272, 329);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(203, 52);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnLogin);
+            this.Name = "FrmMenu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
